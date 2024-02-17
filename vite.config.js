266d4e2d2413c54be2bca8cb4afd8a9a1c94959e
HudsonGraeme <me@hudsongrae.me>
2024-02-17 15:52:42 +0000

    Set root path relative for IPFS

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
 import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  plugins: [{enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},react()],
+	root: './',
+	plugins: [
+		{ enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */) },
+		react(),
+	],
 })

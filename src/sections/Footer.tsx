8f811a0420d8bc04e84da6ae963d0a656780940f
HudsonGraeme <me@hudsongrae.me>
2024-02-17 16:30:53 +0000

    Improve link UX

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,43 +1,45 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Button, HStack, Link } from '@chakra-ui/react'
 
+interface IFooterLink {
+	name: string
+	href: string
+}
+
+const Links: IFooterLink[] = [
+	{
+		name: 'HTTP',
+		href: 'https://hudsongrae.me',
+	},
+	{
+		name: 'IPFS',
+		href: 'ipfs://QmcgLcWX7GReZFYzXevWaGUpCYNX8U4rZerGWPMsVLfpFP',
+	},
+	{
+		name: 'IPNS',
+		href: 'ipns://k51qzi5uqu5dkivw1ezrzlpf6naa09ohwk269mzoleigr2ia9ftq7ahcrhf85l',
+	},
+	{
+		name: 'ENS',
+		href: 'https://hudsongraeme.eth',
+	},
+]
+
 const Footer = () => {
 	return (
-		<HStack w='full' justify='center' align='center'>
-			<HStack>
+		<HStack overflow='hidden' maxW='100vw'>
+			{Links.map(({ name, href }: IFooterLink) => (
 				<Button
+					px={2}
 					variant='ghost'
 					as={Link}
 					rightIcon={<ExternalLinkIcon />}
-					href='https://hudsongrae.me'
+					href={href}
+					key={name}
 				>
-					View via HTTP
+					{name}
 				</Button>
-				<Button
-					variant='ghost'
-					as={Link}
-					rightIcon={<ExternalLinkIcon />}
-					href='ipfs://QmcgLcWX7GReZFYzXevWaGUpCYNX8U4rZerGWPMsVLfpFP'
-				>
-					View on IPFS
-				</Button>
-				<Button
-					variant='ghost'
-					as={Link}
-					rightIcon={<ExternalLinkIcon />}
-					href='ipns://k51qzi5uqu5dkivw1ezrzlpf6naa09ohwk269mzoleigr2ia9ftq7ahcrhf85l'
-				>
-					View via IPNS
-				</Button>
-				<Button
-					variant='ghost'
-					as={Link}
-					rightIcon={<ExternalLinkIcon />}
-					href='https://hudsongraeme.eth'
-				>
-					View ENS
-				</Button>
-			</HStack>
+			))}
 		</HStack>
 	)
 }
